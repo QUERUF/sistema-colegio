9d3a1bc010fecaf762785736bee88f7bb558d584
QUERUF <71291893+QUERUF@users.noreply.github.com>
2026-09-08 17:02:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const path = require('path');
-
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
